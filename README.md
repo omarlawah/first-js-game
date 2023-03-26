bd3727b76b56380d21c340f16b157a0ba83fb585
omarlawah <omarxxxxx135@gmail.com>
2023-03-26 17:22:10 +0000

    unifinished js file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rock-paper-scissors
+
+this is my first javascript project, in this project i made a rock paper scissors game.
+feel free to copy the codemif you wanted to. But please make sure you give me credits for it by typing my name in the README.md file as a reference or simply link my code file to yours.
+
+Thank you.
---
+++ scripts.js
@@ -0,0 +1,13 @@
+    /* 
+    let newParagraph= document.getElementById("intro");
+    console.log(newParagraph)
+    let pContent= newParagraph.textContent;
+    console.log(pContent);
+    */
+
+    /*
+    let div= document.querySelector('.container');
+    let divContent = div.innerHTML;
+    console.log(divContent);
+    let a= document.querySelector('#playerResult').textContent ="player scores:";
+    */
