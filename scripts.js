bd1fd9ebc9b377c7865c94cf6b0f4cc0b3b9290e
omarlawah <omarxxxxx135@gmail.com>
2023-04-02 00:01:51 +0000

    not-finished js file

--- scripts.js
+++ scripts.js
@@ -1,9 +1,68 @@
-let rock = document.querySelector('.Rock');
-let paper = document.querySelector('.Paper');
-let scissors = document.querySelector('.Scissor');
+let Rock = document.querySelector('.Rock');
+let Paper = document.querySelector('.Paper');
+let Scissor = document.querySelector('.Scissor');
+/* 
+document.createElement('p');
+let Results = document.querySelector('.results');
+Results.appendChild('p');
+*/  
 
-let choices=[rock, paper, scissors];
 
-function computerPlay(){
-    Math.trunc(Math.random()*3);
-}
+let choices=['Rock' , 'Paper' , 'Scissor'];
+
+
+
+ 
+
+
+ function computerPlay(){
+
+        return  choices[Math.floor(Math.random()*3)];
+};
+
+
+
+
+let playerChoice0 = Rock;
+playerChoice0.addEventListener('click',function(){
+    computerPlay;
+    let playerCount=0;
+    let computerCount=0;
+    document.querySelector('#playerResult').textContent= `you chose: ${choices[0]}`;
+    document.querySelector('#computerResult').textContent = `the computer chose: ${computerPlay()}`;
+    if(computerPlay===choices[0]){
+        document.querySelector('.playerPoints').textContent=(`player scores: ${playerCount++}`);
+        document.querySelector('.computerPoints').textContent=(`computer scores:  ${computerCount++}`);
+    }else if(computerPlay===choices[1]){
+        document.querySelector('.playerPoints').textContent=(` player scores: ${playerCount}`);
+        document.querySelector('.computerPoints').textContent=(` computer scores: ${computerCount++}`);
+    } 
+    else if(computerPlay===choice[2]){
+        document.querySelector('.playerPoints').textContent=(` player scores: ${playerCount++}`);
+        document.querySelector('.computerPoints').textContent=(` computer scores: ${computerCount}`);
+    } 
+});
+
+
+
+ /*rock.addEventListener('click', function(){
+    document.querySelector('#computerResult').textContent = `the computer chose: `+`${computerPlay()}`;
+});*/
+
+
+
+
+
+/*
+Paper.addEventListener('click', function(){
+    document.querySelector('#computerResult').textContent = `the computer chose: `+`${computerPlay()}` 
+});
+
+ let playerChoice2 = Scissor.addEventListener('click', function(){
+    document.querySelector('#playerResult').textContent= `you chose: ${choices[2]}`;
+});
+
+Scissor.addEventListener('click', function(){
+    document.querySelector('#computerResult').textContent = `the computer chose:`+`${computerPlay()}` 
+});
+*/
